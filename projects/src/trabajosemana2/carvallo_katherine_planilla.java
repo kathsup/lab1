85914e83497cec12d561cfc760f71da5ec39f9aa
katherine <katherinencarvallo22@gmail.com>
2024-10-18 10:36:18 -0600

    variables

---
+++ projects/src/trabajosemana2/carvallo_katherine_planilla.java
@@ -0,0 +1,40 @@
+
+package trabajosemana2;
+
+import java.util.Scanner;
+
+public class carvallo_katherine_planilla {
+     public static void main(String[] args){
+    
+    String nombreempleado;
+    int horastrabajo;
+    double tarifahora,salariosemanal;
+    
+    Scanner entrada = new Scanner(System.in).useDelimiter("\n");
+    
+        System.out.println("----- Boleta del Empleado -------");    
+        System.out.println("Ingrese nombre completo: ");
+        nombreempleado = entrada.next();
+        
+        System.out.println("Ingrese horas laboradas en el mes: ");
+        horastrabajo = entrada.nextInt();
+       
+        System.out.println("Ingrese tarifa por hora: ");
+        tarifahora = entrada.nextDouble();
+    
+        salariosemanal = ((horastrabajo*tarifahora)/4);
+    
+    
+        System.out.println("----- Boleta del Empleado -------");
+        System.out.println("Nombre del Empleado: "+nombreempleado);
+        System.out.println("Hora de Trabajo Mensual: "+horastrabajo);
+        System.out.println("Tarifa por Hora : Lps."+tarifahora);
+        System.out.println("Salario del Empleado Semanal: "+salariosemanal);
+        
+        
+    
+    
+    
+    }
+    
+}
