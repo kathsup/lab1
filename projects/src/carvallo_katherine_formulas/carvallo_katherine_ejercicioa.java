27db1b3b48ebd334ccaaec97dfbae95c51f7d664
katherine <katherinencarvallo22@gmail.com>
2024-10-18 10:59:15 -0600

    creación de variables y formula

---
+++ projects/src/carvallo_katherine_formulas/carvallo_katherine_ejercicioa.java
@@ -0,0 +1,20 @@
+
+package carvallo_katherine_formulas;
+
+public class carvallo_katherine_ejercicioa {
+   public static void main(String[] args){
+   
+   double nominadorA=3;
+    double nominadorB=4;
+    double denominadorC=2;
+    double denominadorD=3;
+    double resul;
+  
+   
+   resul = ((nominadorA/denominadorC)+(nominadorB/denominadorD));
+   
+       System.out.println("el resultado es: "+resul);
+   
+   
+   } 
+}
--- projects/src/trabajosemana2/carvallo_katherine_clics.java
+++ projects/src/trabajosemana2/carvallo_katherine_clics.java
@@ -1,15 +1,26 @@
 
 package trabajosemana2;
 
+import java.util.Scanner;
+
 public class carvallo_katherine_clics {
     public static void main(String[] args){
     
      double sesenta = 0.30;
      double cien = 0.25; 
      double veinte = 0.80;
+     int clics;
+         Scanner entrada = new Scanner(System.in);
+
      
-     
-     
+        System.out.println("Ingresar cantidad de clics para calcular: ");
+        clics = entrada.nextInt();
+        
+        if (clics<=20){
+        
+        
+        
+        }
     
     
     
