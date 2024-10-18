e906bc3be89bab1518dea33fa67c9af9a28be70c
katherine <katherinencarvallo22@gmail.com>
2024-10-18 11:32:07 -0600

    creación de variable y formula
    ejercicio D

---
+++ projects/src/carvallo_katherine_formulas/carvallo_katherine_ejerciciod.java
@@ -0,0 +1,22 @@
+
+package carvallo_katherine_formulas;
+
+public class carvallo_katherine_ejerciciod {
+  public static void main(String[] args){
+  
+  double nume1 = 1;
+   double deno2 = 2;
+   double num = 7; 
+   double resul; 
+   
+   
+   resul = (num + (nume1/deno2));
+      System.out.println("el resultado es :"+ resul);
+  
+  
+  
+  
+  
+  
+  }  
+}
