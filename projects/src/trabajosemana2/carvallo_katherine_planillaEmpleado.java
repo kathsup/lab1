230bbceae61f045f248ff4cbcc806b7460a4a909
katherine <katherinencarvallo22@gmail.com>
2024-10-18 09:11:02 -0600

    creación de variables, formula e instrucciones

---
+++ projects/src/trabajosemana2/carvallo_katherine_planillaEmpleado.java
@@ -0,0 +1,39 @@
+
+package trabajosemana2;
+
+import java.util.Scanner;
+
+public class carvallo_katherine_planillaEmpleado {
+        public static void main(String[] args){
+    
+    String nombreempleado;
+    int horastrabajo;
+    double tarifahora,salariosemanal;
+    
+    Scanner entrada = new Scanner(System.in);
+    
+        System.out.println("----- Boleta del Empleado -------");    
+        System.out.println("Ingrese nombre completo: ");
+        nombreempleado = entrada.nextLine();
+        
+        System.out.println("Ingrese horas laboradas en el mes: ");
+        horastrabajo = entrada.nextInt();
+       
+        System.out.println("Ingrese tarifa por hora: ");
+        tarifahora = entrada.nextDouble();
+    
+        salariosemanal = ((horastrabajo*tarifahora)/4);
+    
+    
+        System.out.println("----- Boleta del Empleado -------");
+        System.out.println("Nombre del Empleado: "+nombreempleado);
+        System.out.println("Hora de Trabajo Mensual: "+horastrabajo);
+        System.out.println("Tarifa por Hora : Lps."+tarifahora);
+        System.out.println("Salario del Empleado Semanal: "+salariosemanal);
+        
+        
+    
+    
+    
+    }
+}
