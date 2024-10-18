a4e8af2deff5aae5b1aef809e26f9b4202dc1906
katherine <katherinencarvallo22@gmail.com>
2024-10-18 11:28:47 -0600

    creación de variables y formula

--- projects/src/carvallo_katherine_formulas/carvallo_katherine_ejercicioc.java
+++ projects/src/carvallo_katherine_formulas/carvallo_katherine_ejercicioc.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package carvallo_katherine_formulas;
 
-/**
- *
- * @author Lenovo
- */
 public class carvallo_katherine_ejercicioc {
+    public static void main(String[] args){
     
+   double nume1 = 1;
+   double deno2 = 2;
+   double num = 7; 
+   double resul; 
+   
+   
+   resul  = ((nume1/deno2)+num);
+        System.out.println("el resultado es: "+resul);
+   
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
 }
