31d7c898093be0299a1aa55724ae0dbed0138c17
katherine <katherinencarvallo22@gmail.com>
2024-10-18 08:26:32 -0600

    subir archivos

---
+++ projects/src/trabajosemana2/carvallo_katherine_variables.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajosemana2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class carvallo_katherine_variables {
+    
+}
