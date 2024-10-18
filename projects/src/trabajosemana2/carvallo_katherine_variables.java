38af9cad1e6e399f6edac1ac4f83435db0eb872d
katherine <katherinencarvallo22@gmail.com>
2024-10-18 08:44:29 -0600

    boleta del empleado

--- projects/src/trabajosemana2/carvallo_katherine_variables.java
+++ projects/src/trabajosemana2/carvallo_katherine_variables.java
@@ -1,13 +1,30 @@
 
 package trabajosemana2;
 
+import java.util.Scanner;
+
 public class carvallo_katherine_variables {
     public static void main(String[] args){
     
     String nombreempleado;
     int horastrabajo;
-    double tarifahora;
+    double tarifahora,salariosemanal;
     
+    Scanner entrada = new Scanner(System.in);
+    
+        System.out.println("----- Boleta del Empleado -------");    
+        
+    
+    
+    
+    
+        System.out.println("----- Boleta del Empleado -------");
+        System.out.println("Nombre del Empleado: "+nombreempleado);
+        System.out.println("Hora de Trabajo Mensual: "+horastrabajo);
+        System.out.println("Tarifa por Hora : Lps."+tarifahora);
+        System.out.println("Salario del Empleado Semanal: "+salariosemanal);
+        
+        
     
     
     
