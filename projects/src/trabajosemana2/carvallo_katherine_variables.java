3d779bc7e460f0af52ffc89bc86e814f90cbd672
katherine <katherinencarvallo22@gmail.com>
2024-10-18 08:37:03 -0600

    creación de variables

--- projects/src/trabajosemana2/carvallo_katherine_variables.java
+++ projects/src/trabajosemana2/carvallo_katherine_variables.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package trabajosemana2;
 
-/**
- *
- * @author Lenovo
- */
 public class carvallo_katherine_variables {
+    public static void main(String[] args){
     
+    String nombreempleado;
+    int horastrabajo;
+    double tarifahora;
+    
+    
+    
+    
+    }
 }
