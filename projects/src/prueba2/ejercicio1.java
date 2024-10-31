ce958496527c1ccf95e66374503ae24c327a9318
katherine <katherinencarvallo22@gmail.com>
2024-10-31 10:41:25 -0600

    procedimiento

--- projects/src/prueba2/ejercicio1.java
+++ projects/src/prueba2/ejercicio1.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package prueba2;
 
-/**
- *
- * @author Lenovo
- */
+import java.util.Scanner;
+
 public class ejercicio1 {
+public static void main(String[]args){
+
+
+//costo total 
+// ingresar precio de cada producto y cantidad de productos comprados 
+//preguntar cuantos productos va a registrar y calcular el costo total 
+
+     Scanner entrada = new Scanner(System.in); 
+     int cantidadregistrar; 
+     int[] precio; 
+     int[] cantidad; 
+     
+    System.out.println("Ingrese la cantidad de productos a registrar: ");
+    cantidadregistrar = entrada.nextInt();
+
+   double[] productos = new double[cantidadregistrar]; 
+   double[] costo = new double[cantidadregistrar]; 
+  
+       //System.out.println("Ingrese el precio de los productos : ");
+       
+       for (int i = 0; i < cantidadregistrar; i++) {
+           System.out.println((i+1)+" favor ingresar el precio: ");
+           productos[i] = entrada.next().charAt(0);
+           
+           System.out.println((i+1)+" favor ingresar la cantidad: ");
+           costo[i] = entrada.next().charAt(0);
+           
+       }
+       
+       System.out.println("el precio total es: ");
+       for (int i = 0; i < cantidadregistrar; i++) {
+           System.out.print(productos[i]+" "); 
+       }
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 
 }
