6fc7a3e1d7dfd58c01df430fc7d02742bbc69a2a
katherine <katherinencarvallo22@gmail.com>
2024-11-01 08:33:35 -0600

    Creación de menu

--- projects/src/Carvallo_Katherine_Estructura/Carvallo_Katherine_Estructura.java
+++ projects/src/Carvallo_Katherine_Estructura/Carvallo_Katherine_Estructura.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Carvallo_Katherine_Estructura;
 
-/**
- *
- * @author Lenovo
- */
+import java.util.Scanner;
+
 public class Carvallo_Katherine_Estructura {
-    
-}
+ public static void main(String[]args){
+ 
+ Scanner entrada = new Scanner(System.in);
+ int opcion; 
+ do{
+     System.out.println("**Menu**");
+     System.out.println("1. Palabra Alreves");
+     System.out.println("2. Número perfecto");
+     System.out.println("3. primos ");
+     System.out.println("4. votaciones");
+     System.out.println("5. salir ");
+     opcion = entrada.nextInt(); 
+     
+     switch(opcion){
+      
+         case 1: System.out.println("PALABRA ALREVES");
+         
+     
+     
+     
+     }
+     
+ }
+ while(opcion != 5); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ }  //cierra main 
+}//cierra class
