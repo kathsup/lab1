d8aa9f4e51423e30db99f82b91bd3194f9d19ab0
katherine <katherinencarvallo22@gmail.com>
2024-11-05 10:11:37 -0600

    Create semana5.java

---
+++ projects/src/progratrabajosemana5/semana5.java
@@ -0,0 +1,29 @@
+//ejercicio1
+package progratrabajosemana5;
+
+import java.util.Scanner;
+
+public class semana5 {
+    public static void main(String[] args){
+    
+    int cantidadVentas; 
+    Scanner entrada = new Scanner(System.in); 
+    
+    System.out.println("Ingresar numero de ventas: ");
+    cantidadVentas = entrada.nextInt(); 
+    
+    
+    for (int i = 0; i < cantidadVentas; i++) {
+    
+        System.out.println("Ingrese el total de cada venta: ");
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    }
+}
