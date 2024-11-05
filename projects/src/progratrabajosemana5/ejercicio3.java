3f08ba74643dbb08fb2fe0da0e10af1836fa71f1
katherine <katherinencarvallo22@gmail.com>
2024-11-05 10:57:52 -0600

    Update ejercicio3.java

--- projects/src/progratrabajosemana5/ejercicio3.java
+++ projects/src/progratrabajosemana5/ejercicio3.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package progratrabajosemana5;
 
-/**
- *
- * @author Lenovo
- */
 public class ejercicio3 {
-    
+   public static void main(String[] args){
+   
+  
+        for (int i = 2; i <= 100; i++) {
+            boolean esPrimo = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+            if (esPrimo) {
+                System.out.print(i + ", ");
+            }
+   
+        }
+   
+   
+   
+   
+   
+   
+   
+   } 
 }
