cf040a2f3d959bc7848a522c30b8a03f29dc3a99
katherine <katherinencarvallo22@gmail.com>
2024-11-05 10:21:56 -0600

    Update semana5.java

--- projects/src/progratrabajosemana5/semana5.java
+++ projects/src/progratrabajosemana5/semana5.java
@@ -7,7 +7,9 @@ public class semana5 {
     public static void main(String[] args){
     
     int cantidadVentas; 
+    double total; 
     Scanner entrada = new Scanner(System.in); 
+    double suma = 0; 
     
     System.out.println("Ingresar numero de ventas: ");
     cantidadVentas = entrada.nextInt(); 
@@ -16,10 +18,14 @@ public class semana5 {
     for (int i = 0; i < cantidadVentas; i++) {
     
         System.out.println("Ingrese el total de cada venta: ");
+        total = entrada.nextDouble(); 
+        suma = suma+total; 
         
+        /*int [] total = new int [cantidadVentas]; 
+        System.out.println(total);*/
     }
-    
-    
+        System.out.println("el total es: "+suma);
+        
     
     
     
