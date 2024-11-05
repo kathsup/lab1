3e75be70b4554008adbce02b3ddaddb6431acf15
katherine <katherinencarvallo22@gmail.com>
2024-11-05 11:00:44 -0600

    comentarios

--- projects/src/progratrabajosemana5/segundotrabajo.java
+++ projects/src/progratrabajosemana5/segundotrabajo.java
@@ -15,18 +15,18 @@ public class segundotrabajo {
         System.out.print("1. Lunes\n2. Martes\n3. Miercoles\n4. Jueves\n5. Viernes\n6. Sabado\n7. Domingo\n8. Salir");
         dia = entrada.nextInt(); 
         
-        
+        //que siga hasta que ponga el n8
         while(dia != 8){
-        
+        //si elige entre estos dias es laboral 
         if(dia==1 || dia==2 || dia== 3 || dia== 4 || dia==5){
         
             System.out.println("DIA LABORAL");
-        }
+        }//si no, no lo es 
         else{
         
             System.out.println("DIA NO LABORAL");
         }
-        
+        //vuelve a preguntarlo 
         System.out.println("Ingrese el numero del día a elegir: ");
         System.out.println("1. Lunes\n2. Martes\n3. Miercoles\n4. Jueves\n5. Viernes\n6. Sabado\n7. Domingo\n8. Salir");
         dia = entrada.nextInt(); 
--- projects/src/progratrabajosemana5/semana5.java
+++ projects/src/progratrabajosemana5/semana5.java
@@ -10,11 +10,11 @@ public class semana5 {
     double total; 
     Scanner entrada = new Scanner(System.in); 
     double suma = 0; 
-    
+    //ingresar cuantas veces va a moverse el ciclo 
     System.out.println("Ingresar numero de ventas: ");
     cantidadVentas = entrada.nextInt(); 
     
-    
+    //la cantidad que se repite y hacer la sumatoria 
     for (int i = 0; i < cantidadVentas; i++) {
     
         System.out.println("Ingrese el total de venta: "+(i+1));
